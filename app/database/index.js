fb1bfdf5f4c2120c77efbc4691ca5c2475fa3e7c
chiragaroraji <chiragarora609@gmail.com>
2018-07-18 17:06:00 +0530

    #2 commit

--- app/database/index.js
+++ app/database/index.js
@@ -6,12 +6,13 @@ var logger 		= require('../logger');
 
 // Connect to the database
 // construct the database URI and encode username and password.
-var dbURI = "mongodb://" + 
-			encodeURIComponent(config.db.username) + ":" + 
-			encodeURIComponent(config.db.password) + "@" + 
-			config.db.host + ":" + 
-			config.db.port + "/" + 
+var dbURI = "mongodb://" + config.db.host + "/" + 
 			config.db.name;
+			//encodeURIComponent(config.db.username) + ":" + 
+			//encodeURIComponent(config.db.password) + "@" + 
+			//config.db.host + ":" + 
+			//config.db.port + "/" + 
+			//config.db.name;
 Mongoose.connect(dbURI);
 
 // Throw an error if the connection fails
